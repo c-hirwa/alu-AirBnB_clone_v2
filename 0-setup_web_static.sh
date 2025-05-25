f36f2784159096069a7ac8b5a70f298ae0ee17c7
c-hirwa <c.hirwa@alustudent.com>
2025-05-25 17:07:13 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,4 +15,4 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # give ownership
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+
