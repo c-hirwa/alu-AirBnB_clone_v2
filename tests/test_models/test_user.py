76a728ab4c3656d951da370645946b51c9a92623
c-hirwa <c.hirwa@alustudent.com>
2025-05-25 18:57:12 +0200

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,8 +4,8 @@ from tests.test_models.test_base_model import TestBaseModel
 from models.user import User
 
 
-class test_User(TestBaseModel):
-    """ """
+class TestUser(TestBaseModel):
+    """ Test for user"""
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -16,20 +16,23 @@ class test_User(TestBaseModel):
     def test_first_name(self):
         """ """
         new = self.value()
+        new.first_name = "Abissa"
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
+        new.last_name = "Abissa"
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
-        new = self.value() 
-        new.email = "j.mugisha2@alustudent.com"
+        new = self.value()
+        new.email = "a.sani@alustudent.com"
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
+        new.password = "123aashja"
         self.assertEqual(type(new.password), str)
